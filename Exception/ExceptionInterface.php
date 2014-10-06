53a6b690eec54180286a555b8aa51a4a9ff8db90
Stephan Wentz <swentz@brainbits.net>
2014-10-06 14:24:38 +0200

    exception fixes, media cache and element change management

--- Exception/ExceptionInterface.php
+++ Exception/ExceptionInterface.php
@@ -9,11 +9,11 @@
 namespace Phlexible\Bundle\SiterootBundle\Exception;
 
 /**
- * Runtime exception
+ * Exception interface for siteroot bundle
  *
  * @author Stephan Wentz <sw@brainbits.net>
  */
-class RuntimeException extends \RuntimeException implements ExceptionInterface
+interface ExceptionInterface
 {
 
 }
